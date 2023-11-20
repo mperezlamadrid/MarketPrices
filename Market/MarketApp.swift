42cb52c8c72e8cad803dff0e56e0bae815a0dc77
Manuel Perez <mperez@wawand.co>
2023-11-20 14:27:11 -0500

    Initial Commit

---
+++ Market/MarketApp.swift
@@ -0,0 +1,17 @@
+//
+//  MarketApp.swift
+//  Market
+//
+//  Created by Manuel Perez on 20/11/23.
+//
+
+import SwiftUI
+
+@main
+struct MarketApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
